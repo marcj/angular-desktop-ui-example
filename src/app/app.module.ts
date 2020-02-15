646f1f6dd5be7fbdff520c6e6122b25c9993ea20
Marc J. Schmidt <marc@marcjschmidt.de>
2020-02-15 22:33:14 +0100

    added angular-desktop-ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,48 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {
+  DuiAppModule,
+  DuiButtonModule,
+  DuiCheckboxModule,
+  DuiDialogModule,
+  DuiEmojiModule,
+  DuiFormComponent,
+  DuiIconModule,
+  DuiInputModule,
+  DuiListModule,
+  DuiRadioboxModule,
+  DuiSelectModule,
+  DuiSliderModule,
+  DuiTableModule,
+  DuiWindowModule,
+} from '@marcj/angular-desktop-ui';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DuiAppModule.forRoot(),
+    DuiWindowModule.forRoot(),
+
+    DuiCheckboxModule,
+    DuiButtonModule,
+    DuiInputModule,
+    DuiFormComponent,
+    DuiRadioboxModule,
+    DuiSelectModule,
+    DuiIconModule,
+    DuiListModule,
+    DuiTableModule,
+    DuiButtonModule,
+    DuiDialogModule,
+    DuiEmojiModule,
+    DuiSliderModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
